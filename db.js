de0878a580375f7ff437ab33d5fbb8dd9bdbec5a
bl0xc0la <BloxColaYT@outlook.com>
2026-05-14 23:00:43 +0100

    Refactor connectDB function syntax and error handling

--- db.js
+++ db.js
@@ -1,13 +1,19 @@
 const mongoose = require("mongoose");
 
-const connectDB = async () => {
-    try {
+async function connectDB(){
+
+    try{
+
         await mongoose.connect(process.env.MONGO_URL);
+
         console.log("MongoDB connected");
-    } catch (err) {
-        console.log("DB error:", err);
-        process.exit(1);
+
+    }catch(err){
+
+        console.log(err);
+
     }
-};
+
+}
 
 module.exports = connectDB;
