b8a7e80bc36a3d60e7af5012f608e3d2cffe810b
bl0xc0la <BloxColaYT@outlook.com>
2026-05-14 23:04:50 +0100

    Refactor connectDB function for module export

--- db.js
+++ db.js
@@ -1,19 +1,10 @@
 const mongoose = require("mongoose");
 
-async function connectDB(){
-
-    try{
-
+module.exports = async function connectDB() {
+    try {
         await mongoose.connect(process.env.MONGO_URL);
-
         console.log("MongoDB connected");
-
-    }catch(err){
-
-        console.log(err);
-
+    } catch (err) {
+        console.log("MongoDB error:", err);
     }
-
-}
-
-module.exports = connectDB;
+};
