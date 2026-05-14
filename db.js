5e0f650f2a886969664ab41e6a215231b4f2be40
bl0xc0la <BloxColaYT@outlook.com>
2026-05-14 22:28:36 +0100

    Refactor DB connection error logging

--- db.js
+++ db.js
@@ -1,21 +1,13 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-
     try {
-
         await mongoose.connect(process.env.MONGO_URL);
-
         console.log("MongoDB connected");
-
     } catch (err) {
-
-        console.log("DB connection error:", err);
-
+        console.log("DB error:", err);
         process.exit(1);
-
     }
-
 };
 
 module.exports = connectDB;
