b37ceff20d5906494de170d9e9460166c28c6adf
bl0xc0la <BloxColaYT@outlook.com>
2026-05-16 16:12:50 +0100

    Integrate MongoDB for game data storage

--- server.js
+++ server.js
@@ -2,38 +2,40 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const mongoose = require('mongoose');
 const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Simple in-memory storage for multiplayer positions
-let players = {};
+// Connect to DB for saving published games
+mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/cubeworld');
+
+const Game = mongoose.model('Game', new mongoose.Schema({
+    name: String,
+    creator: String,
+    parts: Array,
+    date: { type: Date, default: Date.now }
+}));
 
 io.on('connection', (socket) => {
-    socket.on('join-game', (data) => {
-        players[socket.id] = {
-            id: socket.id,
-            username: data.username,
-            x: 0, y: 1, z: 0,
-            color: '#ffcc00'
-        };
-        socket.emit('current-players', players);
-        socket.broadcast.emit('new-player', players[socket.id]);
+    // Send existing games on connect
+    Game.find({}).then(games => socket.emit('sync-games', games));
+
+    socket.on('publish-game', async (data) => {
+        const newGame = await Game.create({
+            name: data.name,
+            creator: data.creator,
+            parts: data.parts
+        });
+        const allGames = await Game.find({});
+        io.emit('sync-games', allGames);
     });
 
-    socket.on('move', (pos) => {
-        if (players[socket.id]) {
-            players[socket.id].x = pos.x;
-            players[socket.id].y = pos.y;
-            players[socket.id].z = pos.z;
-            socket.broadcast.emit('player-moved', players[socket.id]);
-        }
-    });
-
-    socket.on('disconnect', () => {
-        delete players[socket.id];
-        io.emit('player-left', socket.id);
+    socket.on('join-game', (user) => {
+        socket.broadcast.emit('player-joined', user);
     });
 });
 
-http.listen(10000, () => console.log('CubeWorld Engine Online'));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
+
+http.listen(10000, () => console.log('Polytoria-Style Engine Online'));
